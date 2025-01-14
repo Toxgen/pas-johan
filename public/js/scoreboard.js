cf0a28fcd5a96b39d443d9cb0f28c935de170763
toxgen <hydro_toxgen@outlook.com>
2025-01-13 19:44:05 -0500

    does this word

--- public/js/scoreboard.js
+++ public/js/scoreboard.js
@@ -134,9 +134,9 @@ function drawStartScreen() {
 
   var startButton = createButton("GET READY!");
   startButton.position(width / 2 - 200, height / 2);
-  startButton.style(
-    "@keyframes test: {from {top: 0px;} to {top: 500px;}} font-family:monospace; font-weight:bold; font-size:60px; padding:10px; border-radius:10px; animation: test 5s infinite;"
-  );
+  // startButton.style(
+  //   "font-family:monospace; font-weight:bold; font-size:60px; padding:10px; border-radius:10px;"
+  // );
   startButton.mousePressed(startNewGame);
   image(startScreen, 0, 0);
 }
